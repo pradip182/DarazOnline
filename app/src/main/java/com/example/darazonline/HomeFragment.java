a711b61b56b48e474ae7b1100644181b81197433
pradip <pradipacharya182@gmail.com>
2020-01-10 13:52:02 +0545

    login almost done

--- app/src/main/java/com/example/darazonline/HomeFragment.java
+++ app/src/main/java/com/example/darazonline/HomeFragment.java
@@ -3,17 +3,13 @@ package com.example.darazonline;
 import android.content.Context;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.darazonline.URL.Url;
@@ -21,7 +17,6 @@ import com.example.darazonline.adapter.ProductsAdapter;
 import com.example.darazonline.api.Api;
 import com.example.darazonline.model.Products;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
