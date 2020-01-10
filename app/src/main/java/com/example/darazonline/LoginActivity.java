72f51932b010311232b72714db47b072010f652b
pradip <pradipacharya182@gmail.com>
2020-01-10 15:09:41 +0545

    final..

---
+++ app/src/main/java/com/example/darazonline/LoginActivity.java
@@ -0,0 +1,25 @@
+package com.example.darazonline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
